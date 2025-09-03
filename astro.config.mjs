b20139ee86d15a699575d1c0c349ab63425300cd
dixydres <jeanjose.muller@etu.unilim.fr>
2025-09-03 09:46:30 +0200

    page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dixydres.github.io',
+  base: '/Projet-1/',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
